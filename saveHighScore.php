9e36975f9236fdcc8821ef0a36897f592f73d9c2
Julian4806 <90309801+julian4806@users.noreply.github.com>
2023-10-29 12:11:47 +0100

    Added prepared statements

--- saveHighScore.php
+++ saveHighScore.php
@@ -7,9 +7,15 @@ $player = $_GET['player'];
 $score = $_GET['score'];
 
 $my_date = date("Y-m-d H:i:s");
-$query = " INSERT INTO `player` (name , date) VALUES('$player', '$my_date') ";
-$result = mysqli_query($conn, $query);
 
-$player_id = $conn->insert_id;
-$query = " INSERT INTO `score` (player_id, score, playdate) VALUES('$player_id', '$score', '$my_date') ";
-$result = mysqli_query($conn, $query);
+$query = "INSERT INTO `player` (name, date) VALUES(?, ?)";
+$stmt = $conn->prepare($query);
+$stmt->bind_param("ss", $player, $my_date);
+$stmt->execute();
+
+$player_id = $stmt->insert_id;
+
+$query = "INSERT INTO `score` (player_id, score, playdate) VALUES(?, ?, ?)";
+$stmt = $conn->prepare($query);
+$stmt->bind_param("iis", $player_id, $score, $my_date);
+$stmt->execute();
